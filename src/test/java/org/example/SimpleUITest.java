5cc3ad1ec6e07e5cba5c9821e768244d5ba1b60e
Zakhar <zakhar.kolo@gmail.com>
2024-09-29 16:44:11 +0300

    UI additional

--- src/test/java/org/example/SimpleUITest.java
+++ src/test/java/org/example/SimpleUITest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
@@ -10,7 +11,7 @@ import static com.codeborne.selenide.Selenide.element;
 public class SimpleUITest {
 
     @Test
-    public void userTest() {
+    public void userCanCreateAccount() {
         Selenide.open("https://parabank.parasoft.com/parabank/register.htm");
 
         SelenideElement firstName = element(Selectors.byId("customer.firstName"));
@@ -48,6 +49,44 @@ public class SimpleUITest {
         SelenideElement registerButton = element(Selectors.byValue("Register"));
         registerButton.click();
 
+    }
+
+    @Test
+    public void userCanNotCreateAccountWithNameAndSurnameOnly() {
+        Selenide.open("https://parabank.parasoft.com/parabank/register.htm");
+
+        SelenideElement firstName = element(Selectors.byId("customer.firstName"));
+        firstName.sendKeys("Ivan");
+
+        SelenideElement lastName = element(Selectors.byId("customer.lastName"));
+        lastName.sendKeys("Kuzin");
+
+        SelenideElement registerButton = element(Selectors.byValue("Register"));
+        registerButton.click();
+
+        SelenideElement addressError = element(Selectors.byId("customer.address.street.errors"));
+        addressError.shouldHave(Condition.exactText("Address is required."));
+
+        SelenideElement cityError = element(Selectors.byId("customer.address.city.errors"));
+        cityError.shouldHave(Condition.exactText("City is required."));
+
+        SelenideElement stateError = element(Selectors.byId("customer.address.state.errors"));
+        stateError.shouldHave(Condition.exactText("State is required."));
+
+        SelenideElement zipCodeError = element(Selectors.byId("customer.address.zipCode.errors"));
+        zipCodeError.shouldHave(Condition.exactText("Zip Code is required."));
+
+        SelenideElement ssnError = element(Selectors.byId("customer.ssn.errors"));
+        ssnError.shouldHave(Condition.exactText("Social Security Number is required."));
+
+        SelenideElement usernameError = element(Selectors.byId("customer.username.errors"));
+        usernameError.shouldHave(Condition.exactText("Username is required."));
+
+        SelenideElement passwordError = element(Selectors.byId("customer.password.errors"));
+        passwordError.shouldHave(Condition.exactText("Password is required."));
+
+        SelenideElement repeatedPasswordError = element(Selectors.byId("repeatedPassword.errors"));
+        repeatedPasswordError.shouldHave(Condition.exactText("Password confirmation is required."));
     }
 
 }
