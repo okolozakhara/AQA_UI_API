0938ad97ba98d4e0008ba5c1206cc807a94e940d
okolozakhara <zakhar.kolo@gmail.com>
2024-10-27 22:50:33 +0300

    Update RandomData.java

--- src/test/java/org/example/utils/RandomData.java
+++ src/test/java/org/example/utils/RandomData.java
@@ -3,7 +3,16 @@ package org.example.utils;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class RandomData {
+    
     public static String randomString() { // Мутод для рандомизации данных
         return "test_" + RandomStringUtils.randomAlphabetic(10);
+    }
+    
+    public static String randomInt() {
+        return "test" + RandomStringUtils.randomAlphanumeric(9);
+    }
+
+    public static String generatePassword() {
+        return randomString();
     }
 }
